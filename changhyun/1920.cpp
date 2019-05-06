db94e68ece8e20badb8d8ea15a021dafdad6dcf1
changhyun/kayden <43057466+ckdgus0204@users.noreply.github.com>
2019-05-06 16:27:17 +0900

    Update 1920.cpp

--- changhyun/1920.cpp
+++ changhyun/1920.cpp
@@ -7,37 +7,59 @@
 M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.*/
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
+int A[100001] = { 0 };
+int B[100001] = { 0 };
+void Solution(int n, int key) {
+
+	int start = 0;
+	int end = n - 1;
+	int mid;
+
+	while (end - start >= 0) {
+		mid = (start + end) / 2;
+
+		if (A[mid] == key) {   //key 값이 배열의 중앙 값과 같을때
+			printf("1\n");
+			return;
+
+		}
+		else if (A[mid] > key) { //key 값이 배열의 중앙 값보다 작을때 (왼쪽으로)
+			end = mid - 1;
+
+		}
+		else {  //key 값이 배열의 중앙 값보다 클때 (오른쪽으로)
+			start = mid + 1;
+		}
+	}
+
+	printf("0\n");
+	return;
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int A[100001] = { 0 };
-	int B[100001] = { 0 };
+
 
 	int a;
 	cin >> a;
-	for (size_t i = 0; i < a; i++)
-	{
-		cin >> A[i];
-	}
 
-	int count = 0;
+	for (size_t i = 0; i < a; i++)
+		cin >> A[i];
+	
+	sort(A,A+a);
+
 	int b;
 	cin >> b;
-	for (size_t i = 0; i < b; i++)
-	{
-		cin >> B[i];
-		count = 0;
-		for (size_t j = 0; j < a; j++)
-		{
-			if (A[j] == B[i])
-				count++;
-		}
-		cout << count << "\n";
-	}
 
+	for (size_t i = 0; i < b; i++) {
+		cin >> B[i];
+		Solution(a, B[i]);
+	}
 }
 /* 이렇게 짰는데 시간 초과가 떴다. 입력 부분에서 시간을 줄여줘야 할거같은데 어떻게 줄일지 감이 안온다.
 첫번째 배열을 입력받을때 아에 배열크기를 int범위로 해서 입력을 받고 싶었는데 배열크기에는 한계가 있어서 불가능했다.*/
+// 입력말고 B행렬을 입력받고 탐색을 하는 부분에서 시간을 줄여주는 문제였다.
